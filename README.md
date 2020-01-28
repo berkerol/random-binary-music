987d1ed612b9343781a96e3676974d16726ecf9c
Berk Erol <berk.erol@boun.edu.tr>
2020-01-28 12:17:39 +0300

    Add customizations & README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Random Binary Music
+
+[![Sonarcloud Status](https://sonarcloud.io/api/project_badges/measure?project=berkerol_random-binary-music&metric=alert_status)](https://sonarcloud.io/dashboard?id=berkerol_random-binary-music)
+[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg)](https://github.com/berkerol/random-binary-music/issues)
+[![semistandard](https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg)](https://github.com/Flet/semistandard)
+[![ECMAScript](https://img.shields.io/badge/ECMAScript-latest-brightgreen.svg)](https://www.ecma-international.org/ecma-262)
+[![license](https://img.shields.io/badge/license-GNU%20GPL%20v3.0-blue.svg)](https://github.com/berkerol/random-binary-music/blob/master/LICENSE)
+
+Listen to the music of random binary numbers. You can customize notes, periods and bpm. Press _R_ to reset settings to defaults. Pause by pressing _P_. Made with [Tone.js](https://tonejs.github.io).
+
+[![button](watch.png)](https://berkerol.github.io/random-binary-music/rbm.html)
+
+## Contribution
+
+Feel free to [contribute](https://github.com/berkerol/random-binary-music/issues) according to the [semistandard rules](https://github.com/Flet/semistandard) and [latest ECMAScript Specification](https://www.ecma-international.org/ecma-262).
+
+## Distribution
+
+You can distribute this software freely under [GNU GPL v3.0](https://github.com/berkerol/random-binary-music/blob/master/LICENSE).
--- rbm.js
+++ rbm.js
@@ -1,10 +1,14 @@
-/* global Tone createButtonGroup */
-const notes = ['F#3', 'G#3', 'A#3', 'C4', 'D4', 'E4', 'F#4', 'G#4', 'A#4', 'C5', 'D5', 'E5']; // frequency (300) or pitch notation (C4)
-const periods = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.0, 0.9, 0.8, 0.7, 0.6, 0.5]; // second (1.2) or note name (4n) or ticks (192i)
-const range = 2 ** notes.length;
+/* global Tone createButtonGroup createModalButton createModal */
+const defaultNotes = ['F#3', 'G#3', 'A#3', 'C4', 'D4', 'E4', 'F#4', 'G#4', 'A#4', 'C5', 'D5', 'E5']; // frequency (300) or pitch notation (C4)
+const defaultPeriods = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.0, 0.9, 0.8, 0.7, 0.6, 0.5]; // second (1.2) or note name (4n) or ticks (192i)
+const defaultMinBpm = 60;
+const defaultMaxBpm = 140;
+const notes = new Array(12);
+const periods = new Array(12);
+let minBpm;
+let maxBpm;
 
-const minBpm = 60;
-const maxBpm = 140;
+const range = 2 ** defaultNotes.length;
 
 const synths = [];
 
@@ -12,20 +16,50 @@ let playing = false;
 let prev = convertNumberToBinaryString(generateRandomInteger(range));
 let number = convertNumberToBinaryString(generateRandomInteger(range));
 
-const buttonElements = [['success', 'playOrPause()', 'p', 'play', '<u>P</u>lay']];
+const modalElements = [[['1st note', 'note1', 'text'], ['2nd note', 'note2', 'text'], ['3rd note', 'note3', 'text'], ['4th note', 'note4', 'text'], ['5th note', 'note5', 'text'], ['6th note', 'note6', 'text']], [['7th note', 'note7', 'text'], ['8th note', 'note8', 'text'], ['9th note', 'note9', 'text'], ['10th note', 'note10', 'text'], ['11th note', 'note11', 'text'], ['12th note', 'note12', 'text']], [['1st period', 'period1', 'text'], ['2nd period', 'period2', 'text'], ['3rd period', 'period3', 'text'], ['4th period', 'period4', 'text'], ['5th period', 'period5', 'text'], ['6th period', 'period6', 'text']], [['7th period', 'period7', 'text'], ['8th period', 'period8', 'text'], ['9th period', 'period9', 'text'], ['10th period', 'period10', 'text'], ['11th period', 'period11', 'text'], ['12th period', 'period12', 'text']], [['Min Bpm', 'minBpm', 40, 200, 'number'], ['Max Bpm', 'maxBpm', 40, 200, 'number']]];
+const buttonElements = [['success', 'playOrPause()', 'p', 'play', '<u>P</u>lay'], ['info', '', 's', 'cog', '<u>S</u>ettings']];
 const buttonGroup = createButtonGroup('btn-group btn-group-lg', buttonElements);
-const button = buttonGroup.children[0];
+const playButton = buttonGroup.children[0];
 const bitsDiv = document.createElement('div');
 bitsDiv.id = 'bits';
-document.getElementsByClassName('container')[0].children[0].appendChild(buttonGroup);
+document.getElementsByClassName('container')[0].children[0].appendChild(createModalButton(buttonGroup, 1));
+createModal(modalElements);
 document.getElementsByClassName('container')[0].appendChild(bitsDiv);
-for (let i = 0; i < notes.length; i++) {
+document.addEventListener('keyup', function (e) {
+  if (e.keyCode === 80) {
+    playOrPause();
+  } else if (e.keyCode === 82) {
+    resetInputs();
+  }
+});
+for (let i = 0; i < defaultNotes.length; i++) {
   synths.push(new Tone.Synth().toDestination());
   bitsDiv.appendChild(document.createElement('span'));
 }
+resetInputs();
 Tone.Transport.bpm.value = (minBpm + maxBpm) / 2;
 Tone.Transport.scheduleRepeat(update, '192i');
 playOrPause();
+
+function resetInputs () {
+  minBpm = defaultMinBpm;
+  maxBpm = defaultMaxBpm;
+  for (let i = 0; i < notes.length; i++) {
+    document.getElementById(`note${i + 1}`).value = (notes[i] = defaultNotes[i]);
+    document.getElementById(`period${i + 1}`).value = (periods[i] = defaultPeriods[i]);
+  }
+  document.getElementById('minBpm').value = minBpm;
+  document.getElementById('maxBpm').value = maxBpm;
+}
+
+window.save = function () {
+  for (let i = 0; i < notes.length; i++) {
+    notes[i] = document.getElementById(`note${i + 1}`).value;
+    periods[i] = document.getElementById(`period${i + 1}`).value;
+  }
+  minBpm = +document.getElementById('minBpm').value;
+  maxBpm = +document.getElementById('maxBpm').value;
+};
 
 function update (time) {
   for (let i = 0; i < notes.length; i++) {
@@ -45,18 +79,18 @@ function update (time) {
 function playOrPause () {
   if (playing) {
     playing = false;
-    button.innerHTML = '<i class="fas fa-play"></i> <u>P</u>lay';
+    playButton.innerHTML = '<i class="fas fa-play"></i> <u>P</u>lay';
     Tone.Transport.stop();
   } else {
     playing = true;
-    button.innerHTML = '<i class="fas fa-pause"></i> <u>P</u>ause';
+    playButton.innerHTML = '<i class="fas fa-pause"></i> <u>P</u>ause';
     Tone.Transport.start();
   }
 }
 
 function convertNumberToBinaryString (n) {
   let s = '';
-  for (let i = 0; i < notes.length; i++) {
+  for (let i = 0; i < defaultNotes.length; i++) {
     s = ((n >> i) & 1) + s;
   }
   return s;
